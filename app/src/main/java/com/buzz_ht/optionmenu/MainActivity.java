615b12d8cc2c3dc682f43e4a224cf0270fe73eaa
buzz-ht53 <akshayr.java@gmail.com>
2021-08-26 18:55:37 +0530

    First commit

---
+++ app/src/main/java/com/buzz_ht/optionmenu/MainActivity.java
@@ -0,0 +1,46 @@
+package com.buzz_ht.optionmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.about:
+                Toast.makeText(MainActivity.this, "ABOUT", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.refer:
+                Toast.makeText(MainActivity.this, "REFER", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rate:
+                Toast.makeText(MainActivity.this, "RATE", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
